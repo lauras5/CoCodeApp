34afaa0e6836bee180c8b83f706d62a1245f26d1
Laura Salcido <idlaurasalcido@gmail.com>
2018-03-13 21:31:08 -0700

    test 3

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -52,7 +52,6 @@ var initApp = function () {
 
     // on click event for sign out button
     $('#signOutBTN').on('click', function () {
-        console.log("Hi")
         firebase.auth().signOut();
     });
 
